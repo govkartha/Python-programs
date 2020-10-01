e1103ac3d9451a3cfc33e137a96ec26d0c2a1aaa
Philipg99 <32796112+Philipg99@users.noreply.github.com>
2020-10-01 15:15:32 +0530

    alternate memoization soultion

--- collaz.py
+++ collaz.py
@@ -12,3 +12,26 @@ for i in range(1,1000000):
         high=count
         num=i
 print(num)
+
+
+#alternate memoization soultion
+memo = [-1]*1000000
+memo[1] = 0
+
+high = 0
+num = -1
+for i in range(1,1000000):    
+    count = 0
+    n=i
+    while n > 1000000 or memo[n] == -1:
+        count+=1
+        if n%2 == 0:
+            n/=2
+        else:
+            n=3*n+1
+         
+    memo[i] = memo[n]+ count
+    if memo[i] >high:
+        high=memo[i]
+        num=i
+print(num)
