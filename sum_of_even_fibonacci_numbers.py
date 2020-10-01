2cab6f79616325f4d548a72741e232d7d05a14ab
Philipg99 <32796112+Philipg99@users.noreply.github.com>
2020-10-01 15:04:24 +0530

    alternate solution

--- sum_of_even_fibonacci_numbers.py
+++ sum_of_even_fibonacci_numbers.py
@@ -6,3 +6,14 @@ while (a < n):
         total += a
     a, b = b, a+b  
 print(total)
+
+#alternate solution
+
+a, b = 0, 1
+total = 0
+
+while a<n:
+    total+=a
+    a, b = a + 2*b, 2*a + 3*b
+    
+print(total)
