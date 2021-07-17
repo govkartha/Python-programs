91ab9d5076a5518295af03eabe1177fa47e2c9bd
Govind Kartha <44610432+govkartha@users.noreply.github.com>
2021-07-17 13:35:32 +0530

    Update lattice.py

--- lattice.py
+++ lattice.py
@@ -1,4 +1,6 @@
+#Count the number of unique paths to travel from the top left to the bottom right of a lattice of size n*n.
+
 import math
-n=20
+n=int(input())
 result=(math.factorial(2*n))/(math.factorial(n)*math.factorial(n))
 print(result)
